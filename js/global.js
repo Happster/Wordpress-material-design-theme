9921fbe14230f02666d9a28e7577c08275f6b90a
Mantas Malukas <mmalukas@icloud.com>
2015-01-18 00:40:43 +0100

    Material design wordpress theme
    wordpress theme based on Google’s material design.
    More at http://materialdesignblog.com

---
+++ js/global.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+// Load Flexslider
+    $(".flexslider").flexslider({
+        animation: "slide",
+        controlNav: false,
+        prevText: "",
+        nextText: "",
+        smoothHeight: true   
+    });
+});
