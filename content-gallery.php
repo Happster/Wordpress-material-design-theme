75769da2e63620d64686d4f76d2a075a8c758721
Mantas Malukas <Happster@users.noreply.github.com>
2015-01-22 23:01:51 +0100

    content-gallery.php

---
+++ content-gallery.php
@@ -0,0 +1,38 @@
+<article id="post-<?php the_ID(); ?>" <?php if ( is_single() ) : post_class(); else: post_class('well page active'); endif; ?>>
+	    <div class="featured-media">	
+		
+		<?php material_flexslider('post-image'); ?>
+		
+	</div> <!-- /featured-media -->
+
+	<header class="entry-header">
+		<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			endif;
+		?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			/* translators: %s: Name of current post */
+			the_content( sprintf(
+				__( 'Read More', 'Material' ),
+				the_title( '<span class="screen-reader-text">', '</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'material' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'material' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	
+</article><!-- #post-## -->
