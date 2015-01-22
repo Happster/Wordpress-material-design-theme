f3ac1504b083fcc16e3cea44e0a90c13165b366b
Mantas Malukas <Happster@users.noreply.github.com>
2015-01-22 22:58:06 +0100

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+get_header(); ?>
+
+	<div id="primary" class="content-area well page active padding">
+		<main id="main" class="site-main" role="main">
+
+			<section class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'material' ); ?></h1>
+				</header><!-- .page-header -->
+
+			</section><!-- .error-404 -->
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
