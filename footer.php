0239b9d3e4eda0a2b014bb25b0573d5cbb4e194b
Mantas Malukas <Happster@users.noreply.github.com>
2015-01-22 23:12:36 +0100

    footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+
+   </div>
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer padding" role="contentinfo">
+		<div class="site-info">
+			     <a href="<?php echo esc_url( __( 'https://twitter.com/hercusmonte', 'material' ) ); ?>"><?php printf( __( 'Follow me @hercusmonte', 'material' ), '' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
