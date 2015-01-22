1c7622d375f9905501d316084bfb771fc126f63c
Mantas Malukas <Happster@users.noreply.github.com>
2015-01-22 23:03:32 +0100

    Create content-page.php

---
+++ content-page.php
@@ -0,0 +1,27 @@
+<article id="post-<?php the_ID(); ?>">
+		<?php if ( has_post_thumbnail() ) : ?>	
+			<?php the_post_thumbnail('post-image'); ?>
+			
+	<?php endif; ?>
+
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'material' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'material' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php edit_post_link( __( 'Edit', 'material' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->' ); ?>
+
+</article><!-- #post-## -->
